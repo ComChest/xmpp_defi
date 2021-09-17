750cf6a6b40f08aa87eb45d183d61d13b4c111bf
Caleb <sturg103@mail.chapman.edu>
2021-09-16 20:20:14 -0400

    JID module expanded and storage module outlined.
    Resource class taken out of jid file and expanded all sjid signing functionality. Storage model outline begun with sqlite base used except with web apps where the plan is to have it be pulled from IndexedDB and loaded into memory.

--- lib/src/jid/jid.dart
+++ lib/src/jid/jid.dart
@@ -1,38 +1,102 @@
-import 'package:pointycastle/export.dart';
-import "package:pointycastle/pointycastle.dart";
+import 'dart:html';
+import 'package:cryptography/cryptography.dart';
 import 'dart:math';
 import 'dart:convert';
+import 'package:xmpp_defi/resource.dart';
+
+import 'package:cryptography/dart.dart';
+
 
 class JID{
   String user;
   String domain;
-  String resource;
+  Resource _resource;
+  List<Resource> _selfResources = [];
+  Map<String, List<Resource>> contactsSJIDs = Map();
+  Map<String, List<Resource>> contactsFJIDs =  Map();
+  final signatureAlgorithm = Ed25519();
 
-  JID(this.user, this.domain, {this.resource = 'comchest'}) {
-    this.resource = resource + Random.secure().toString()
+  JID(this.user, this.domain, this._resource) {}
+
+  getJID(){
+    return user + "@" + domain + "/" + _resource;
   }
-
-  AuthenticationResult(user, domain)
 }
 
 class SocialJID extends JID{
-  SocialJID(user, domain, {String resource = 'comchest'}) : super(user, domain) {
-    this.resource = resource + Random.secure().toString()
+
+  SocialJID(String user, String domain, Resource Resource) : super(user, domain, Resource) {
+
+  }
+}
+
+MakeSJID(String user, String domain, {resourceID = "Comchest"}){
+  return SocialJID(user, domain, Resource(resourceID + Random.secure().toString()));
+}
+
+class MySJID extends SocialJID{
+  MySJID(String user, String domain, this._selfKeyPair, this._userKeyPair, {String resource = 'Comchest'}) : super(user, domain) {
+  }
+
+  getPubSelfKey(){
+    return _selfKeyPair.extractPublicKey();
+  }
+
+  getPubUserKey(){
+    return _userKeyPair.extractPublicKey();
+  }
+
+  signSelf(self){
+    return signatureAlgorithm.sign([self], keyPair: _selfKeyPair);
+  }
+
+  signUser(user){
+    return signatureAlgorithm.sign([user], keyPair: _userKeyPair);
+  }
+
+}
+
+class MasterSJID extends MySJID{ //ByDefault encrypt MasterKey with ArgonID of password.
+  SimpleKeyPairData _masterKeyPair;
+  // Sign
+  MasterSJID(user, domain, _selfKeyPair, _userKeyPair, this._masterKeyPair, {String resource = 'Comchest'}) : super(user, domain, _selfKeyPair, _userKeyPair) {
+    this.resource = resource + Random.secure().toString();
+  }
+
+  getPubMasterKey(){
+    return _masterKeyPair.extractPublicKey();
+  }
+
+  signMaster(key){
+      return signatureAlgorithm.sign([key], keyPair: _masterKeyPair);
   }
 }
 
 class FinJID extends JID{
-  int pubkey;
-  late String digestID;
-
-  FinJID(user, domain, this pubkey, {String resource = 'comchest'}) : super(user, domain) {
-    Argon2BytesGenerator argon = Argon2BytesGenerator();
-    this.digestID = "";
-    //this.digestID = argon.deriveKey(inp, , out, outOff).toString()
-
-  }
+  var SecondarySJIDs = new Map();
+  FinJID(user, domain, _resource) : super(user, domain, _resource) {}
 }
 
-void main() {
-  JID c1 = new JID('aeneas', 'akcu.org');
+class MyFJID extends FinJID{
+  int pubkey;
+  String PrimarySJID;
+
+
+  MyFJID(user, domain, this.pubkey, this.PrimarySJID, {resource = 'Comchest'}) : super(user, domain) {
+
+  }
+
+}
+
+MakeFJID(domain, int pubkey, PrimarySJID, {String resourceName = 'ComChest'}) async {
+  final hmac = Hmac.sha256();
+  final message = utf8.encode(domain);
+  final pubKey = SecretKey([pubkey]);
+  final mac = await hmac.calculateMac(message, secretKey: pubKey);
+  return MyFJID(base64.encode(mac.bytes).replaceAll(new RegExp(r'[^\w\s]+'), ''), domain, pubkey, PrimarySJID);
+}
+
+void main() async {
+  var testFJID = await MakeFJID("akcu.org", 313691, "aeneas@akcu.org");
+  print(testFJID.user);
 }
---
+++ lib/src/jid/resource.dart
@@ -0,0 +1,55 @@
+import 'dart:html';
+import 'package:cryptography/cryptography.dart';
+import 'dart:math';
+import 'dart:convert';
+
+class Resource{ //AKA Device
+  String label = 'ComChest';
+  var rng = new Random();
+  int ID = rng.nextInt(pow(2, 32).toInt(); //XEP-0384 Says it must be unique, probably to bareJID but we will check domain too
+  List<String> _selfFJIDs = [];
+  List<String> _selfSJIDs = [];
+  final signatureAlgorithm = Ed25519();
+  var userPublicKeys = new Map();
+  var userKeySignatures = new Map();
+  Resource(){}
+  Resource(this.Label){}
+  Resource(this.Label, this.ID){}
+}
+
+class makeResource(String resourceLabel){
+  return Resource(resourceLabel))
+}
+
+class KeyedResource extends Resource{
+  SimplePublicKey publicKey;
+  Map<List<int>, Signature> msgSignatures = new Map();
+  KeyedResource(String resourceID, this.publicKey) : super(resourceID){}
+}
+
+class ThisResource extends Resource{
+  SimpleKeyPair _selfSignKeyPair;
+  SimpleKeyPair _userSignKeyPair;
+  Map<String, KeyedResource>
+  selfPublicKeys = new Map();
+  var selfKeySignatures = new Map();
+  ThisResource(String resourceID, this._selfSignKeyPair, this._userSignKeyPair) : super(resourceID){
+  }
+
+  getPubSelfKey(){
+    return _selfSignKeyPair.extractPublicKey();
+  }
+
+  getPubUserKey(){
+    return _userSignKeyPair.extractPublicKey();
+  }
+
+  signSelf(self){
+    return signatureAlgorithm.sign([self], keyPair: _selfSignKeyPair);
+  }
+
+  signUser(user){
+    return signatureAlgorithm.sign([user], keyPair: _userSignKeyPair);
+  }
+}
+
